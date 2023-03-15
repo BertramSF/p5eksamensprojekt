3bf42831e4a066816e7b193944fab8a252ca0de1
BertramSF <105633731+BertramSF@users.noreply.github.com>
2023-03-15 11:09:34 +0100

    PointSystem tilføjet

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@
 const Width = 800
 const Height = 750
 
+// Varibler for pointsystem
+let point = 0
+
 // Variabler for Player
 const d = 72
 let x = 800 / 2
@@ -12,33 +15,46 @@ const r = d / 2
 // Variabler for Enemies
 const enemies = []
 const numberOfEnemies = 10
-var Ehastighed = 5
+let Ehastighed = 5
 
 // Funktion for Enemy Spawning
-function generateEnemies(hastighed){
+function generateEnemies(hastighed) {
   for (let index = 0; index < numberOfEnemies; index++) {
-    enemies.push(new Enemy(random(50, Width - 50), -50, 100, 100, 'red', hastighed))
+    enemies.push(
+      new Enemy(random(50, Width - 50), -50, 100, 100, "red", hastighed)
+    );
   }
   enemies[0].drawEnemy = true
-
 }
 
 // Setup af Spillet
-function setup () {
-  createCanvas(Width, Height)
+function setup() {
+  let cnv = createCanvas(Width, Height)
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+
   rectMode(CENTER)
-  console.log(dist2p(0, 0, 3, 4))
+  // console.log(dist2p(0, 0, 3, 4))
   // Kald Enemy Spawning funktion
   generateEnemies(Ehastighed)
-
 }
 
-function draw () {
+function drawScore(){
+  push()
+  textSize(40)
+  fill("white")
+  text("Score: "+ point, Width-200, 40)
+  pop()
+}
+
+function draw() {
   // Player
   background(69)
   circle(x, y, d)
   strokeWeight(5)
   fill(162, 120, 9)
+
 
   // Nyt Enemy efter forrige Enemy er nået halvejs ned på skærmen
   for (let index = 0; index < enemies.length; index++) {
@@ -53,17 +69,23 @@ function draw () {
 
     // Hitbox registrering
     if (dist2p(enemy.Ex, enemy.Ey, x, y) < d / 2 + enemy.Ew / 2) {
-      console.log('Vi ramte en fjende')
+      console.log("Vi ramte en fjende")
       noLoop()
     }
-  }
-  
-  // Forøgelse af Enemy Hastighed
-  if(enemies[numberOfEnemies-1].Ey +50 >= Height){
-    Ehastighed += 5
-    generateEnemies(Ehastighed)
+
+    // point-optælling
+    if(enemy.Ey>=744 && enemy.point === false){
+      point+=1
+      enemy.point = true
+    }
+
   }
 
+  // Forøgelse af Enemy Hastighed
+  if (enemies[numberOfEnemies - 1].Ey + 50 >= Height) {
+    Ehastighed += 5;
+    generateEnemies(Ehastighed)
+  }
 
   // Player Movement
   if (keyIsDown(RIGHT_ARROW || 68)) {
@@ -77,12 +99,13 @@ function draw () {
       x = x - fart
     }
   }
-
+  
+  drawScore()
 }
 
 // Klasse for Enemy
 class Enemy {
-  constructor (Ex, Ey, Ew, Eh, farve, Ehastighed) {
+  constructor(Ex, Ey, Ew, Eh, farve, Ehastighed) {
     this.Ex = Ex
     this.Ey = Ey
     this.Ew = Ew
@@ -90,10 +113,11 @@ class Enemy {
     this.farve = farve
     this.hastighed = Ehastighed
     this.drawEnemy = false
+    this.point = false
   }
 
-// Draw Enemy
-  draw () {
+  // Draw Enemy
+  draw() {
     push()
     fill(this.farve)
     rect(this.Ex, this.Ey, this.Ew, this.Eh)
@@ -102,7 +126,8 @@ class Enemy {
   }
 }
 //Funktion for Afstands mellem to punkter
-function dist2p (x1, y1, x2, y2) {
+function dist2p(x1, y1, x2, y2) {
   const d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
   return d
 }
+
